42fda7edb5dd6989f2e3b90e93b42628a9b30c0a
aostrvkij <aostrvkij04040@yandex.ru>
2023-01-25 21:02:02 +0300

    fps fix

--- library/basic.py
+++ library/basic.py
@@ -9,7 +9,7 @@ import pygame
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, move=None):
         pygame.sprite.Sprite.__init__(self)
-        self.speed = 4
+        self.speed = 20
         self.width, self.height = 2, 10
         self.move = move
         self.move_x = 0
@@ -17,18 +17,22 @@ class Bullet(pygame.sprite.Sprite):
         self.image.fill('yellow')
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
+        self.x, self.y = self.rect.x, self.rect.y
 
     def update(self):
         if self.move:
             self.move_x += 0.05
             if self.move == 'left':
-                self.rect.x -= int(self.move_x)
-                self.rect.y -= self.speed
+                self.x -= int(self.move_x) / FPS
+                self.y -= self.speed / FPS
             if self.move == 'right':
-                self.rect.x += int(self.move_x)
-                self.rect.y -= self.speed
+                self.x += int(self.move_x) / FPS
+                self.y -= self.speed / FPS
         else:
-            self.rect.y -= self.speed
+            self.y -= self.speed / FPS
+        self.rect.x, self.rect.y = self.x, self.y
+        if SCREEN_HEIGHT < self.rect.y < 0:
+            self.kill()
 
 
 bullets = pygame.sprite.Group()
@@ -46,35 +50,37 @@ class Ship(pygame.sprite.Sprite):
         self.info_id = info_id
         self.image = pygame.Surface((30, 30))
         self.image.fill('red')
+        self.x, self.y = 100, 100
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = pos[0], pos[1]
+        self.rect.x, self.rect.y = self.x, self.y
 
     def update(self, keys):
         if keys[pygame.K_w]:
             if self.rect.y - self.speed > 0:
-                self.rect.y -= self.speed
+                self.y -= self.speed / FPS
             else:
                 self.rect.y = 0
         if keys[pygame.K_s]:
             if self.rect.y + self.speed < SCREEN_HEIGHT - self.rect.height:
-                self.rect.y += self.speed
+                self.y += self.speed / FPS
             else:
                 self.rect.y = SCREEN_HEIGHT - self.rect.height
         if keys[pygame.K_a]:
             if self.rect.x - self.speed > 0:
-                self.rect.x -= self.speed
+                self.x -= self.speed / FPS
             else:
                 self.rect.x = 0
         if keys[pygame.K_d]:
             if self.rect.x + self.speed < SCREEN_WIDTH - self.rect.width:
-                self.rect.x += self.speed
+                self.x += self.speed / FPS
             else:
                 self.rect.x = SCREEN_WIDTH - self.rect.width
         if keys[pygame.K_SPACE]:
             self.attack(keys)
+        self.rect.x, self.rect.y = self.x, self.y
 
     def attack(self, keys):
-        if self.bullet_delay == 10:
+        if self.bullet_delay >= 10:
             if keys[pygame.K_a] and keys[pygame.K_d] or not(keys[pygame.K_a] and keys[pygame.K_d]):
                 bullets.add(Bullet(self.rect.x, self.rect.top))
             else:
@@ -84,7 +90,7 @@ class Ship(pygame.sprite.Sprite):
                     bullets.add(Bullet(self.rect.x, self.rect.top, 'right'))
             self.bullet_delay = 0
         elif self.bullet_delay < 10:
-            self.bullet_delay += 1
+            self.bullet_delay += 10 / FPS
 
 
 class Game:
@@ -97,7 +103,7 @@ class Game:
         self.screen = screen
         self.clock = pygame.time.Clock()
         self.ships = pygame.sprite.Group()
-        self.ships.add(Ship((100, 100), 4, 1, 1, 1, 1, 1))
+        self.ships.add(Ship((100, 100), 10, 1, 1, 1, 1, 1))
 
     def menu(self):
         self.run_menu, self.run_game, self.run_settings, self.run_library = True, False, False, False
@@ -126,7 +132,6 @@ class Game:
             bullets.draw(self.screen)
             self.ships.draw(self.screen)
             flip()
-            self.clock.tick(FPS)
             if keys[pygame.K_ESCAPE]:
                 self.run_game = False
                 quit()
